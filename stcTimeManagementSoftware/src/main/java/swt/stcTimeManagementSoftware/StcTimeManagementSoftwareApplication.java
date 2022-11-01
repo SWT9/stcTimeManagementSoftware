8bc0eda5fe16cca9118522a7f291ac32ed059c05
Tom <49721713+Tomformatik@users.noreply.github.com>
2022-11-01 01:37:37 +0100

    Add files via upload
    Project template

---
+++ stcTimeManagementSoftware/src/main/java/swt/stcTimeManagementSoftware/StcTimeManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package swt.stcTimeManagementSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcTimeManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcTimeManagementSoftwareApplication.class, args);
+	}
+
+}
---
+++ stcTimeManagementSoftware/src/test/java/swt/stcTimeManagementSoftware/StcTimeManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package swt.stcTimeManagementSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcTimeManagementSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
