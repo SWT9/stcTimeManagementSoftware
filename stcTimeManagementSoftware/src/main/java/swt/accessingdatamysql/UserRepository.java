1a8b4811c3f6c67506ddc0f305c946419f2f0a7f
Tom <49721713+Tomformatik@users.noreply.github.com>
2022-11-05 13:05:53 +0100

    database access to html

--- stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/UserRepository.java
+++ stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/UserRepository.java
@@ -1,5 +1,6 @@
 package swt.accessingdatamysql;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import swt.accessingdatamysql.User;
@@ -7,6 +8,11 @@ import swt.accessingdatamysql.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+   // @Query("SELECT u FROM user u where u.username = ?")
+   // User findNameByUsername(String username);
+
+    User findByUsername(String username);
 }
--- stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/thirdTry/HomeController.java
+++ stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/thirdTry/HomeController.java
@@ -1,11 +1,29 @@
 package swt.accessingdatamysql.thirdTry;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+import swt.accessingdatamysql.UserRepository;
 
 @Controller
 public class HomeController {
 
+    //private MySessionInfo mySessionInfo;
+    @Autowired
+    private UserRepository userRepository;
+
+	@GetMapping("/mainPage")
+	public String user(Model model) {
+		//model.addAttribute("username", mySessionInfo.getCurrentUser().getUsername());   // attribut which will be adressed
+		
+        model.addAttribute("username", userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).getForname());
+        return "mainPage";  // return name of adressed html file in /templates
+	}
+    
     @GetMapping("/")
     public String index() {
         return "index";
