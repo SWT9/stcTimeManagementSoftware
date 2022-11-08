d4277d892e1aa8e323e2a4a28640978f8cc35e38
Tom <49721713+Tomformatik@users.noreply.github.com>
2022-11-09 00:05:51 +0100

    Implemented data access for our put-in data
    //TODO: give user as attribut and access everything from this object... just one model.addAttribut

--- stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/thirdTry/VacationTimeRepository.java
+++ stcTimeManagementSoftware/src/main/java/swt/accessingdatamysql/thirdTry/VacationTimeRepository.java
@@ -1,16 +1,17 @@
 package swt.accessingdatamysql.thirdTry;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import swt.accessingdatamysql.VacationTime;
-
-
-
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface VacationTimeRepository extends CrudRepository<VacationTime, Integer>{
     VacationTime findById(int id);
+
+    List<VacationTime> findAllByUserId(int userId);
 }
 
 
